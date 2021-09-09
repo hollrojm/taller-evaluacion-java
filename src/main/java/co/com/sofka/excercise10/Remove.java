f2ec82e2e09b75f546b8baf4451e958ab62e372d
Hollman Rojas <hollmanerojas@gmail.com>
2021-09-09 02:39:03 -0500

    feature/ exercise10 Remove Spaces in text

---
+++ src/main/java/co/com/sofka/excercise10/Remove.java
@@ -0,0 +1,25 @@
+package co.com.sofka.excercise10;
+
+import java.util.Scanner;
+
+public final class Remove {
+    Scanner scanner = new Scanner(System.in);
+
+    private String noSpaces ="";
+
+    public Remove() {
+        this.scanner = scanner;
+
+    }
+    public String removeSpaces(){
+        System.out.print("Ingresa una frase de varias palabras: ");
+        String text = scanner.nextLine();
+            for(int i=0;i<text.length();i++){
+                char remove = text.charAt(i);
+                if(remove != ' '){
+                    noSpaces += String.valueOf(remove);
+                }
+            }
+            return noSpaces;
+    }
+}
---
+++ src/main/java/co/com/sofka/excercise10/RunRemoveSpaces.java
@@ -0,0 +1,9 @@
+package co.com.sofka.excercise10;
+
+public final class RunRemoveSpaces {
+    public static void main(String[] args) {
+        Remove remove  = new Remove();
+        System.out.println(remove.removeSpaces());
+
+    }
+}
